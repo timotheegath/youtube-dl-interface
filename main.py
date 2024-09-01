14e2e0451fada5d9979ebae5fa2598339aa7e0eb
Timo <timogath@gmail.com>
2024-09-01 13:47:58 +0200

    Updated pipeline to exclude binaries. added --devmode argument to [pull from main.ui

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from os import getcwd
 
 print(f"Current wd: {getcwd()}")
 print(f"Sys args: {sys.argv}")
-app = DownloaderApplication(sys.argv, root=getcwd(), load_dev_ui=True)
+app = DownloaderApplication(sys.argv, root=getcwd(), load_dev_ui=("--devmode" in sys.argv))
 
 sys.exit(app.exec())
